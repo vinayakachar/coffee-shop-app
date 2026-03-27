8a03697dd51b5d3a4501e658cdfa1a29f288ce67
Vinayaka R <vinayakarachar@gmail.com>
2026-03-27 09:37:09 +0530

    chore: initial project setup and build configuration

---
+++ app/src/main/java/com/developer/coffeeshopapp/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.developer.coffeeshopapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+//Mycolors
+
+val CoffeeBrown = Color (0xFFC47B4D)
+val CreamBeige = Color (0xFFEBD4C6)
+val CharcoalGray = Color (0xFF313131)
+val LightGray = Color (0xFFE1E1E1)
+val IvoryWhite = Color (0xFFF7F0EB)
+val LightBrown = Color (0xFFC67C4E)
