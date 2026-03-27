3a5317aeb43c9994ef934d55267f15a3942cec37
Vinayaka R <vinayakarachar@gmail.com>
2026-03-27 18:29:32 +0530

    feat: create data class for the product

---
+++ app/src/main/java/com/developer/coffeeshopapp/model/Product.kt
@@ -0,0 +1,10 @@
+package com.developer.coffeeshopapp.model
+
+data class Product(
+    val id: Int,
+    val name:String,
+    val description:String,
+    val price:Double,
+    val imageResource:Int
+)
+
