643c930be482c7204d36bc7852e50d01f28acffd
Vinayaka R <vinayakarachar@gmail.com>
2026-03-27 14:44:56 +0530

    feat: add interactive category cards with selection state

---
+++ app/src/main/java/com/developer/coffeeshopapp/Screens/HomeScreen/CategoryCard.kt
@@ -0,0 +1,45 @@
+package com.developer.coffeeshopapp.Screens.HomeScreen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.developer.coffeeshopapp.ui.theme.CreamBeige
+import com.developer.coffeeshopapp.ui.theme.LightBrown
+import com.developer.coffeeshopapp.ui.theme.LightGray
+
+
+@Composable
+fun CategoryCard(
+    text: String,
+    isSelected: Boolean,
+    onSelected: () -> Unit,
+) {
+    Box(
+        modifier = Modifier.width(90.dp)
+            .height(30.dp)
+            .clip(RoundedCornerShape(6.dp))
+            .clickable{onSelected()}
+            .background(color = if (isSelected) LightBrown else LightGray.copy(alpha = 0.6f)),
+        contentAlignment = Alignment.Center,
+    ){
+        Text(text=text,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.SemiBold,
+            maxLines = 1,
+            color = if (isSelected) Color.White else Color.Black,)
+    }
+}
---
+++ app/src/main/java/com/developer/coffeeshopapp/Screens/HomeScreen/HomeScreenCategories.kt
@@ -0,0 +1,42 @@
+package com.developer.coffeeshopapp.Screens.HomeScreen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreenCategories() {
+
+    val categories = listOf(
+        "All items",
+        "Mocha",
+        "Cappuccino",
+        "Espresso",
+        "Americano",
+        "Mocha",
+    )
+
+    var selectedCategory by remember { mutableStateOf(categories.first()) }
+
+    LazyRow(modifier = Modifier.padding(horizontal = 12.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+        items(categories){category->
+            CategoryCard(
+                text = category,
+                isSelected = category == selectedCategory,
+                onSelected = { selectedCategory = category }
+            )
+        }
+
+
+    }
+}
