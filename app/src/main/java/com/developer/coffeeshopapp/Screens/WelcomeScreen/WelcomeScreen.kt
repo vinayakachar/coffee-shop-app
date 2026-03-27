58ea8350248581f473388915572a609c10f14046
Vinayaka R <vinayakarachar@gmail.com>
2026-03-27 10:25:01 +0530

    feat: add Welcome Screen UI

---
+++ app/src/main/java/com/developer/coffeeshopapp/Screens/WelcomeScreen/WelcomeScreen.kt
@@ -0,0 +1,74 @@
+package com.developer.coffeeshopapp.Screens.WelcomeScreen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.developer.coffeeshopapp.R
+import com.developer.coffeeshopapp.ui.theme.LightBrown
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun WelcomeScreen() {
+    Box(modifier = Modifier.fillMaxSize().background(color = Color.Black)){
+
+       Image(painter = painterResource(R.drawable.image_splash),
+           contentDescription = "Welcome Image")
+
+        Column(modifier = Modifier.fillMaxSize().padding(vertical = 70.dp, horizontal = 24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom) {
+
+            Text(text = "Fall in Love with Coffe in Blissful Delight",
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                textAlign = TextAlign.Center,
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(text = "Welcome to out cozy coffe corner,where every up is a delight for you",
+                fontSize = 15.sp,
+                color = Color.LightGray,
+                textAlign = TextAlign.Center)
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            Button(onClick = {}, modifier = Modifier.fillMaxWidth()
+                .height(50.dp)
+                , shape = RoundedCornerShape(8.dp)
+                , colors = ButtonDefaults.buttonColors(
+                    containerColor = LightBrown,
+                    contentColor = Color.White
+                )
+            ) {
+                Text(text = "Get Started",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    )
+            }
+
+        }
+    }
+}
