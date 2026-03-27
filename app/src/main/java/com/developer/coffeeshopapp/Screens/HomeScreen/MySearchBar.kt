f3b698c1fabf61545aadc9b900c66c97be45a4ad
Vinayaka R <vinayakarachar@gmail.com>
2026-03-27 14:21:43 +0530

    feat: implement search bar functionality on HomeScreen

---
+++ app/src/main/java/com/developer/coffeeshopapp/Screens/HomeScreen/MySearchBar.kt
@@ -0,0 +1,77 @@
+package com.developer.coffeeshopapp.Screens.HomeScreen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.developer.coffeeshopapp.R
+import com.developer.coffeeshopapp.ui.theme.LightBrown
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MySearchBar() {
+    var searchText by remember { mutableStateOf("") }
+    Row(modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically) {
+        TextField(
+            value = searchText,
+            onValueChange = {searchText = it},
+            placeholder = { Text(text = "Search Coffee", color = Color.Gray) },
+            leadingIcon = {
+                Icon(painter = painterResource(R.drawable.regular_outline_search),
+                    contentDescription = "Search", modifier = Modifier.size(20.dp),
+                    tint = Color.White)
+            },
+            shape = RoundedCornerShape(topStart = 16.dp,
+                topEnd = 0.dp,
+                bottomStart = 16.dp,
+                bottomEnd = 0.dp),
+            singleLine = true,
+            modifier = Modifier.weight(1f).height(60.dp),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color(0xFF422A24),
+                unfocusedContainerColor = Color(0xFF2A2A2A),
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                cursorColor = Color.LightGray,
+                unfocusedTextColor = Color.White,
+                focusedTextColor = Color.White
+            )
+            )
+
+        Spacer(modifier = Modifier.width(8.dp))
+        IconButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.background(
+                color = LightBrown,
+                shape = RoundedCornerShape(topStart = 0.dp,topEnd = 16.dp,bottomStart = 0.dp,bottomEnd = 16.dp,)
+            ).size(height = 58.dp, width = 50.dp),
+
+        ) {
+            Icon(painter = painterResource(R.drawable.regular_outline_filter),
+                contentDescription = "Filter",
+                tint = Color.White
+            )
+        }
+    }
+}
